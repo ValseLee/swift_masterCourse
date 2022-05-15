e20fd9f91aa062064d39e1bc4cabdb214041d4f0
ValseLee <82270058+ValseLee@users.noreply.github.com>
2022-05-15 21:41:40 +0900

    220515 Daily Study, CS, Swift Basic(constants)

--- swiftBasics/1.variables.swift
+++ swiftBasics/1.variables.swift
@@ -1,3 +1,14 @@
 // variables
+// 숫자로 변수 시작 불가, 대문자로는 하지 말자
+// lowerCamelCase를 컨벤션으로 활용
 var a = 3;
 print("a");
+
+// 이때, a는 데이터 자리에 저장된다.
+// 쉽게 데이터 재할당도 가능하다
+a = 5;
+print("a");
+
+var name: String = "이름";
+// backslash 로 문자열 보간 가능
+print("제 이름은 \(name)");
---
+++ swiftBasics/2.constants.swift
@@ -0,0 +1,4 @@
+// 변하지 않는 데이터(=immutable)
+// JS와 다르게 let으로 상수를 선언한다.
+let NAME: String = "Vetiver";
+print("NAME");
